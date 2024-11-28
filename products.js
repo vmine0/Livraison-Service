6ceb3806c9e010c2639ab5edd8f8d2e841f97261
vmine0 <159934454+vmine0@users.noreply.github.com>
2024-11-28 02:04:10 +0100

    Affichage par 3 ou 4

---
+++ products.js
@@ -0,0 +1,30 @@
+const products = [
+    // Alcool
+    { img: "img/poliakov-20cl.jpg", alt: "Poliakov 20cl", name: "Poliakov 20cl", price: "10.00€", type: "alcool" },
+    { img: "img/poliakov-70cl.jpeg", alt: "Poliakov 70cl", name: "Poliakov 70cl", price: "25.00€", type: "alcool" },
+    { img: "img/J&B-70cl.jpeg", alt: "J&B 70cl", name: "J&B 70cl", price: "30.00€", type: "alcool" },
+    { img: "img/William-70cl.jpeg", alt: "William Lawson's 70cl", name: "William Lawson's 70cl", price: "28.00€", type: "alcool" },
+    { img: "img/JackDaniel-70cl.jpeg", alt: "Jack Daniel's 70cl", name: "Jack Daniel's 70cl", price: "40.00€", type: "alcool" },
+
+    // Boissons
+    { img: "img/cristaline-50cl.jpeg", alt: "Cristaline 50cl", name: "Cristaline 50cl", price: "1.00€", type: "boissons" },
+    { img: "img/boisson-33cl.jpg", alt: "Boisson 33cl", name: "Boisson 33cl", price: "1.60€", type: "boissons" },
+    { img: "img/redbull-25cl.jpeg", alt: "Red Bull 25cl", name: "Red Bull 25cl", price: "3.00€", type: "boissons" },
+    { img: "img/monster-50cl.jpeg", alt: "Monster 50cl", name: "Monster 50cl", price: "3.00€", type: "boissons" },
+
+    // Snack
+    { img: "img/ChipsLays-135g.jpeg", alt: "Chips 135g", name: "Chips 135g", price: "3.00€", type: "snack" },
+    { img: "img/ChipsLays-45g.jpeg", alt: "Chips Lays 45g", name: "Chips Lays 45g", price: "1.50€", type: "snack" },
+    { img: "img/Prince.jpeg", alt: "Prince xg", name: "Prince xg", price: "2.00€", type: "snack" },
+    { img: "img/Granola.jpeg", alt: "Granola xg", name: "Granola xg", price: "2.00€", type: "snack" },
+
+    // Bière
+    { img: "img/Heineken-50cl.jpeg", alt: "Heineken 50cl", name: "Heineken 50cl", price: "2.00€", type: "biere" },
+    { img: "img/Heineken-12x25cl.jpeg", alt: "Heineken 12x25cl", name: "Heineken 12x25cl", price: "24.00€", type: "biere" },
+    { img: "img/LaGoudale-50cl .jpeg", alt: "La Goudale 50cl", name: "La Goudale 50cl", price: "3.00€", type: "biere" },
+    { img: "img/Leffe-50cl.jpeg", alt: "Leffe 50cl", name: "Leffe 50cl", price: "3.00€", type: "biere" },
+
+    // Formule
+    { img: "img/Formule10€.png", alt: "Combo 1", name: "Bouteille 50cl + Flask + Redbull 25cl", price: "10.00€", type: "formule" },
+    { img: "img/Formule20€.png", alt: "Combo 2", name: "Bouteille 70cl + 2 Redbull", price: "20.00€", type: "formule" }
+];
